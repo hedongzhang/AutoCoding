de6902d5323079369eec136900f426f5a6d85f70
hedongzhang <H_D_Zhang@163.com>
2015-07-24 14:48:42 +0800

    AutoCoding V1.0.0

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2014Äê12ÔÂ14ÈÕ
+ *      Author: HDZhang
+ */
+#include<stdlib.h>
+#include"ACConfManager.h"
+#include"ACService.h"
+#include"ACManager.h"
+#include<memory.h>
+#include<map>
+
+using namespace n_acmanager;
+using namespace std;
+
+int main(void) {
+
+	ACManager acManager;
+	system("pause");
+	return 0;
+}
+
+
+
